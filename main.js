bfe90d5a03f49141725b2c5e42deb82204c329fc
AplinXy9plin <evilberry@icloud.com>
2018-10-05 21:05:25 +0700

    Add VK Longpoll

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
+var config = require('./config.json');
+
+var vk_token = config.vk
+
+const { VK } = require('vk-io');
+
+const vk = new VK();
+
+vk.setOptions({
+	token: vk_token,
+	apiMode: 'parallel_selected',
+	webhookPath: '/webhook/secret-path'
+});
+
+const { updates } = vk;
+var vk_app = updates;
+
+vk_app.use(async (context, next) => {
+	if (context.is('message') && context.isOutbox) {
+		return;
+	}
+
+	try {
+		await next();
+	} catch (error) {
+		console.error('Error:', error);
+	}
+});
+
+vk_app.on('message',(message) => {
+    console.log('Новое сообщение:',message.text);
+});
+
+async function run() {
+	if (process.env.UPDATES === 'webhook') {
+		await vk.updates.startWebhook();
+
+		console.log('Webhook server started');
+	} else {
+		await vk.updates.startPolling();
+
+		console.log('VK longpoll started');
+	}
+}
+
+run().catch(console.error);
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
